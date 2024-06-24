962165e3a6e9db291db03200320eced771d1636b
iatoha <iatohaph@gmail.com>
2024-06-24 22:16:49 +0600

    all done

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ function handleContact(event) {
     console.log(email);
     console.log(message);
 
+    const successContainer = document.getElementById("success-container")
+    console.log(successContainer);
+    
+    const nameParagraph = document.createElement("p")
+    nameParagraph.innerText = `Your name : ${name}`;
+    const emailParagraph = document.createElement("p")
+    emailParagraph.innerText = `Your email : ${email}`
+    const messageParagraph = document.createElement("p")
+    messageParagraph.innerText = `Your massage : ${message}`
+    
+    console.log(emailParagraph);
+    console.log(nameParagraph);
+    console.log(messageParagraph);
+    
+    successContainer.appendChild(nameParagraph);
+    successContainer.appendChild(emailParagraph);
+    successContainer.appendChild(messageParagraph)
+
+
     // console.log(event.target.name.value);
     // console.log(event.target.email.value);
 
@@ -17,22 +36,71 @@ function handleContact(event) {
     //  console.log(event.target.name);
     //  console.log(event.target.email);
     //  console.log(event.target.message);
-
-    const successContainer = document.getElementById("success-container")
-    console.log(successContainer);
-
-    const nameParagraph =document.createElement("p")
-    nameParagraph.innerText = `Your name : ${name}`;
-    const emailParagraph = document.createElement("p")
-    emailParagraph.innerText = `Your email : ${email}`
-    const messageParagraph = document.createElement("p")
-    messageParagraph.innerText = `Your massage : ${message}`
-
-    console.log(emailParagraph);
-    console.log(nameParagraph);
-    console.log(messageParagraph);
-
-    successContainer.appendChild(nameParagraph);
-    successContainer.appendChild(emailParagraph);
-    successContainer.appendChild(messageParagraph)    
 }
+
+
+// array of object
+const imageArray = [
+    {
+        url: "./images/projects/image 3.jpg"
+    },
+    {
+        url: "./images/projects/image 5.jpg"
+    },
+    {
+        url: "./images/projects/image 6.jpg"
+    },
+    {
+        url: "./images/projects/image 7.jpg"
+    },
+]
+
+function handleShowMoreProjects() {
+    const projectContainer = document.getElementById("projects");
+
+    for (const item of imageArray) {
+        // console.log(item);
+        const projectDivImage = document.createElement('div');
+        projectDivImage.innerHTML = `
+            <img src="${item.url}" alt="">
+        `
+        projectContainer.appendChild(projectDivImage)
+
+        console.log(projectDivImage);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// const firstUrlObject = imageArray[0];
+// const secondUrlObject = imageArray[1];
+
+// console.log(imageArray);
+// console.log(firstUrlObject);
+// console.log(firstUrlObject.url);
+// console.log(secondUrlObject);
+
+// for (const items of imageArray){
+//     console.log(items);
+// }
+
+// const imagesLength = imageArray.length;
+// console.log(imagesLength);       
+
+// const firstUrl = imageArray[0];
+// console.log(firstUrl);   
+
+// for (let index = 0; index < imagesLength; index++){
+//     console.log(index);
+
+//     console.log(imageArray[index]);
+// }
+
+
